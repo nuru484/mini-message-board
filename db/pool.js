6f2ed4bd68dfdde8f30fab0f1dfdbf7740a2a0e8
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-08-13 00:46:15 +1200

    Changed the pool

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Needed for some cloud-hosted databases
+    rejectUnauthorized: false,
   },
 });
 
