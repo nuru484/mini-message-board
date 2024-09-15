610dc8c72df014cfde16f096b5520a65b311ede2
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-09-16 01:18:55 +1200

    db update

--- db/populatedb.js
+++ db/populatedb.js
@@ -17,15 +17,40 @@ const insertMessagesSQL = `
   ('Alex Johnson', 'Post your message here.')
 `;
 
+const createDeploymentStatusTableSQL = `
+  CREATE TABLE IF NOT EXISTS deployment_status (
+    id SERIAL PRIMARY KEY,
+    task_name VARCHAR(255) UNIQUE,
+    completed BOOLEAN DEFAULT FALSE
+  );
+`;
+
 const populatedb = async () => {
   try {
+    const checkStatusQuery = `SELECT completed FROM deployment_status WHERE task_name = 'tableCreated'`;
+    const statusResult = await pool.query(checkStatusQuery);
+
+    if (statusResult.rows.length > 0 && statusResult.rows[0].completed) {
+      console.log('Database created already completed. Skipping creation.');
+      return;
+    }
+
     // Create table
     await pool.query(createTableSQL);
+    await pool.query(createDeploymentStatusTableSQL);
     console.log('Table created or already exists.');
 
     // Insert messages
     await pool.query(insertMessagesSQL);
     console.log('Database populated successfully.');
+
+    const updateStatusQuery = `
+      INSERT INTO deployment_status (task_name, completed)
+      VALUES ('tableCreated', true)
+      ON CONFLICT (task_name)
+      DO UPDATE SET completed = EXCLUDED.completed;
+    `;
+    await pool.query(updateStatusQuery);
   } catch (err) {
     console.error('Error populating database:', err);
   } finally {
